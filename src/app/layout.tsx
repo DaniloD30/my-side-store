e4392eb1ecd12ed0dd19891a8d4e06c6a6484674
Danilo Dias <danilolive30@hotmail.com>
2024-09-19 15:13:15 -0300

    feat: globals.css, my side favicon, font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { type_second } from "@/functions/fonts";
 export const metadata: Metadata = {
   title: "My Side Store",
   description: "Store",
@@ -13,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body className={type_second.variable}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>My Side</h1>
+      <h1 className="title">My Side</h1>
     </main>
   );
 }
