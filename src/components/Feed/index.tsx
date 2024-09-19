fb8d1275a3652c37cbc8962c2c9cbbcdf670af6a
Danilo Dias <danilolive30@hotmail.com>
2024-09-19 20:58:03 -0300

    feat: component feed base

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,11 @@
+import Filters from "../Filters";
+import Products from "../Products";
+
+export default function Feed() {
+  return (
+    <>
+      <Filters />
+      <Products />
+    </>
+  );
+}
