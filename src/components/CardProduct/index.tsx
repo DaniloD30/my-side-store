bcefd2378394d8629bebc1af6bd8f26eb92a6d6b
Danilo Dias <danilolive30@hotmail.com>
2024-09-19 23:15:59 -0300

    feat: product card

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,15 +1,33 @@
 import Image from "next/image";
 import { Product } from "./types";
-
-//TODO:  Card Product: menos a imagem, o nome, o preço e uma breve descrição, botão
-//       adicionar no carrinho{" "}
+import Link from "next/link";
+import styles from "./card-product.module.css";
 
 //TODO: Toast de Produto adicionado no carrinho ou algum feedback
+//TODO: Componetizar Button
 
 export default function CardProduct({ product }: { product: Product }) {
   return (
-    <div style={{padding: 10}}>
-      <Image src={product.image} alt={product.title} width={500} height={500} />
+    <div className={styles.card}>
+      <Link href={`/product/${product.id}`}>
+        <h5 className={styles.title}>{product.title}</h5>
+        <div className={styles.image}>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={220}
+            height={220}
+            sizes="80vw"
+          />
+        </div>
+        <h5 className={styles.description}>{product.description}</h5>
+        <div className={styles.containerTitle}>
+          <h5 className={styles.title}>${product.price}</h5>
+        </div>
+        <div className={styles.containerTitle}>
+          <button>Adicionar ao carrinho</button>
+        </div>
+      </Link>
       {/* <h4>{product.title}</h4> */}
     </div>
   );
--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,17 +1,18 @@
 import productsGet from "@/actions/products-get";
 import CardProduct from "../CardProduct";
 import { ResponseProduct } from "../CardProduct/types";
+import styles from "./products.module.css";
 
 export default async function Products() {
   const data: ResponseProduct = await productsGet();
 
   //TODO: Products contem uma lista de card Products e a paginação
   //TODO: Loading
-  console.log(data);
+
   return (
-    <div>
+    <div className={styles.containerProducts}>
       {data.products.map((item, index) => (
-        <CardProduct key={index} product={item} />
+        <CardProduct key={item.id + index} product={item} />
       ))}
     </div>
   );
