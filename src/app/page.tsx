51784056e33359f3dbfb7a5d3c8242d88f8269df
Danilo Dias <danilolive30@hotmail.com>
2024-09-19 21:00:29 -0300

    feat: component home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+//TODO: Input Search
+//TODO: Select Filter
+//TODO: Feed > Filters > Cards Products com Layout Grid (De 4 e ir diminuindo conforte a tela diminui)
+//TODO: Paginação
+
+import Feed from "@/components/Feed";
+
 export default function Home() {
   return (
     <section className="container mainContainer">
       <h1 className="title">My Side</h1>
+      <Feed />
     </section>
   );
 }
