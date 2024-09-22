be5f6c5d74b4eb2b86248ce19a7117c5656fe066
Danilo Dias <danilolive30@hotmail.com>
2024-09-22 20:59:28 -0300

    feat: cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+
+import { useProduct } from "@/context/ProductContext";
+import style from "./cart.module.css";
+import Image from "next/image";
+
+//TODO: your cart is empty
+//TODO: Verificar a quantidade de itens do carrinho
+//TODO: Remover item do carrinho
+
 export default function Cart() {
-  return <h1 className="title">Cart</h1>;
+  const { productsCart } = useProduct();
+  return (
+    <section className="container mainContainer">
+      <div className={style.containerTitleAndButton}>
+        <div className={style.containerTitle}>
+          <h1 className="title">Carrinho</h1>
+          <h5>{`(${productsCart.length} Produtos)`}</h5>
+        </div>
+        <div>
+          <button className={style.selectCategory}>
+            Voltar para os produtos
+          </button>
+        </div>
+      </div>
+      {productsCart.map((product) => (
+        <div className={style.containerCardCart}>
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={200}
+            height={200}
+            sizes="80vw"
+          />
+          <div>
+            <div className={style.titleProduct}>
+              <h4>{product.title}</h4>
+            </div>
+            <div className={style.containerText}>
+              <h4>Marca:</h4>
+              <p>{product.brand.toUpperCase()} </p>
+            </div>
+            <div className={style.containerText}>
+              <h4>Modelo:</h4>
+              <p>{product.model.toUpperCase()} </p>
+            </div>
+            <div className={style.containerText}>
+              <h4>Cor:</h4>
+              <p>{product.color.toUpperCase()} </p>
+            </div>
+            <div className={style.containerText}>
+              <h4>Preço:</h4>
+              <p>${product.price} </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,9 +2,13 @@ import { productGet } from "@/actions/products-get";
 import { Product, ResponseSingleProduct } from "@/components/CardProduct/types";
 import ProductDetail from "@/components/ProductDetail";
 
-export default async function ProductDetailContainer({ params }: { params: Product }) {
+export default async function ProductDetailContainer({
+  params,
+}: {
+  params: Product;
+}) {
   const data: ResponseSingleProduct = await productGet(params.id);
-
+  //TODO: Loading
   return (
     <section className="container mainContainer">
       <h1 className="title">Detalhes do produto - {params.id}</h1>
