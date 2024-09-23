d95ddc1277a953e7fd3d421a25954bb30ca8fa43
Danilo Dias <danilolive30@hotmail.com>
2024-09-23 00:41:02 -0300

    feat: remove title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-//TODO: Input Search
-//TODO: Select Filter
-//TODO: Feed > Filters > Cards Products com Layout Grid (De 4 e ir diminuindo conforte a tela diminui)
-//TODO: Paginação
-
 import Feed from "@/components/Feed";
 
 export default function Home() {
   return (
     <section className="container mainContainer">
-      <h1 className="title">My Side</h1>
+      <h1 className="title">Produtos</h1>
       <Feed />
     </section>
   );
